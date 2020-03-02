a072fa990730b634638e9e292228c4c4864b3eaf
amandamurillo <amandasmurillo@gmail.com>
2020-03-02 09:45:56 -0800

    adding package.json to run tests and solved capitalize

---
+++ AlgoCasts-StephenGrider/capitalize/index.js
@@ -0,0 +1,31 @@
+// --- Directions
+// Write a function that accepts a string.  The function should
+// capitalize the first letter of each word in the string then
+// return the capitalized string.
+// --- Examples
+//   capitalize('a short sentence') --> 'A Short Sentence'
+//   capitalize('a lazy fox') --> 'A Lazy Fox'
+//   capitalize('look, it is working!') --> 'Look, It Is Working!'
+
+
+function capitalize(str) {
+  let arr = [];
+
+  str.split(' ').forEach(function(word){
+    arr.push(word[0].toUpperCase() + word.slice(1));
+  });
+
+  return arr.join(' ');
+}
+
+module.exports = capitalize;
+
+
+// function capitalize(str) {
+//   let words = [];
+
+//   for (let word of str.split(' ')) {
+//     words.push(word[0].toUpperCase() + word.slice(1));
+//   }
+//   return words.join(' ');
+// }
