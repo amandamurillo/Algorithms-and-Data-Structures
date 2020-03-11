2bde810861cb6e8f378a8ad524699a481da5b956
amandamurillo <amandasmurillo@gmail.com>
2020-03-11 12:11:34 -0700

    bst with comments

--- AlgoCasts-StephenGrider/bst/index.js
+++ AlgoCasts-StephenGrider/bst/index.js
@@ -20,30 +20,37 @@ class Node {
     this.right = null;
   }
 
-  insert (data) {
-    if (data < this.data && this.left) {
+  insert(data) {
+    //less than, pass it off recursively
+    if(data < this.data && this.left) {
       this.left.insert(data);
     } else if (data < this.data) {
       this.left = new Node(data);
-    } else if( data > this.data && this.right) {
-      this.right.insert(data); 
-    } else if (data > this.data) {
+    } else if (data > this.data && this.right) {
+      this.right.insert(data);
+    } else if (data >this.data) {
       this.right = new Node(data);
     }
   }
 
-  contains(data) {
+  //contains is a comparison at every node
+  //if so, cut in half, dont have to search R side
 
+  contains(data) {
     if (this.data === data) {
       return this;
     }
 
     if (this.data < data && this.right) {
       return this.right.contains(data);
-    } else if (this.data >data && this.left) {
+    } else if (this.data > data && this.left) {
       return this.left.contains(data);
-    }
+      }
 
     return null;
   }
 }
+ 
+module.exports = Node;
+
+  
