4750c627498e9ad077b24b958dfdeae3880af1cf
amandamurillo <amandasmurillo@gmail.com>
2020-06-01 19:35:52 -0700

    fixing typo

--- AlgoCasts-StephenGrider/fib/index.js
+++ AlgoCasts-StephenGrider/fib/index.js
@@ -9,11 +9,10 @@
 //   fib(4) === 3
 
 function fib (n) {
-
 //the first two numbers are 0 and 1 in the sequence. The sequence computations begin at f(2) because that is the first time you can add the two numbers  
 
   //first two are a given
- const results = [0,1]
+ const result = [0,1]
 
  // i is the tracking of positions
  for (let i = 2; i <= n; i++) {
@@ -29,3 +28,29 @@ return result[n]
 module.exports = fib;
 
 
+function memoize(fn) {
+
+  const cache = {};
+  return function (...args) {
+    if (cache[args]) {
+      return cache[args]
+    }
+
+    const result = fn.apply(this, args);
+    cache[args] = result;
+
+    return result;
+  }
+
+}
+
+function slowFib(n) {
+  
+  if (n > 2) {
+    return n;
+  }
+
+  return fib(n - 1) + fib(n -2);
+}
+
+const fib = memoize(slowFib)
